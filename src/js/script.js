99c72d5951d06aa8d0ae5d30c1be33c2b3983d03
deazerer <nasibullin1234@mail.ru>
2022-05-22 17:06:20 +0300

    stylized a modal window and effects

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,29 @@
 let modal = document.getElementById('myModal');
 let openbtn = document.getElementById('openmodal');
 let modalOverlay = document.getElementById('myModalOverlay');
-let videoframe = document.getElementById('videoframe');
+let closebtn = document.getElementById('closeModal');
+
+function toggleVideo(state) {     
+    let iframe = modal.getElementsByTagName("iframe")[0].contentWindow;   
+    modal.style.display = state == 'hide' ? 'none' : '';
+    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
+}
+
+function toLayOpacity(){
+    closebtn.style.opacity = "1";
+}
+function toDelayOpacity(){
+    closebtn.style.opacity = "0.3";
+}
 
 openbtn.onclick = function(){
+    toggleVideo();
     modal.style.display = "flex";
     modal.style.opacity = "1";
 };
 modalOverlay.onclick = function(){
     modal.style.display = "none";
-}
-window.onclick = function(event){
-    if(event.target == modal){
-        modal.style.display = "none";
-    }
+    toggleVideo('hide');
 };
+
