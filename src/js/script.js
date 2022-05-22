4b8c0efe670f09341cbb2122bd6d73ccbb9dc33c
deazerer <nasibullin1234@mail.ru>
2022-05-22 13:48:11 +0300

    made a base of modal window

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded',()=>{
- 
-})
+let modal = document.getElementById('myModal');
+let openbtn = document.getElementById('openmodal');
+let modalOverlay = document.getElementById('myModalOverlay');
+let videoframe = document.getElementById('videoframe');
+
+openbtn.onclick = function(){
+    modal.style.display = "flex";
+    modal.style.opacity = "1";
+};
+modalOverlay.onclick = function(){
+    modal.style.display = "none";
+}
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+};
