14eb02400fb551bc3ac0775713c10928034e3a2a
deazerer <nasibullin1234@mail.ru>
2022-05-24 22:26:19 +0300

    made some little fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 let modal = document.getElementById('myModal');
 let openbtn = document.getElementById('openmodal'); 
-let modalOverlay = document.getElementById('myModalOverlay');
+let VideoModalOverlay = document.getElementById('myModalOverlay');
 let closebtn = document.getElementById('closeModal');
 let burgermenu = document.getElementById('BurgerLinks');
 let ModalBurger = document.getElementById('burgerModal');
 let ModalBurgerOverlay = document.getElementById('burgerModalOverlay');
+let Burger = document.getElementById('Burger');
 
 function toggleVideo(state) {     
     let iframe = modal.getElementsByTagName("iframe")[0].contentWindow;   
@@ -27,7 +28,7 @@ openbtn.onclick = function(){
     modal.style.display = "flex";
     modal.style.opacity = "1";
 };
-modalOverlay.onclick = function(){
+VideoModalOverlay.onclick = function(){
     modal.style.display = "none";
     allowScroll();
     toggleVideo('hide');
@@ -43,12 +44,16 @@ function allowScroll(){
 
 burgermenu.addEventListener("click", function(){
     denyScroll();
+    Burger.style.width = '30px';
+    Burger.style.transition = '0.3s';
     ModalBurger.classList.add('active');
     ModalBurgerOverlay.classList.add('active');
     ModalBurgerOverlay.style.position = "fixed";
 });
 ModalBurgerOverlay.onclick = function(){
     allowScroll();
+    Burger.style.width = '15px';
+    Burger.style.transition = '0.3s';
     ModalBurger.classList.remove('active');
     ModalBurgerOverlay.classList.remove('active');
     ModalBurgerOverlay.style.position = "absolute";
